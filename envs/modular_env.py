18f3da064a54ec00b5448f50705b2c748bb2d22a
erik.fischer98 <erik.fischer@campus.tu-berlin.de>
2023-04-17 11:13:26 +0200

    created program stub, todo: Implement abstact methods

--- envs/modular_env.py
+++ envs/modular_env.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
-from typing import List, Tuple, Union, Optional
+from typing import List, Tuple, Union, Optional, Dict, Any
 import numpy as np
 from pathlib import Path
 from stable_baselines3.common.vec_env.base_vec_env import *
+from gym.utils import seeding
 
 
 class ModularEnv(VecEnv):
@@ -10,91 +11,15 @@ class ModularEnv(VecEnv):
         self.asset_path = asset_path  # Path to assets used in simulation
         self.headless = headless  # True if the simulation will not be rendered, otherwise false 
         self.step_size = step_size  # Amount of time passing each time .step() is called
+        self.env_data: Dict[int, Dict[str, Any]] = {}  # Env data saved in a dict
 
-        obs = self.reset()
+        # parse observation and action space
+        obs = self.reset()[0]
         actions = self.get_robot_dof_limits()
         print(obs, actions)
 
         raise "Engine constructor not implemented"
         super().__init__(num_envs, None, None)
-
-    @abstractmethod
-    def set_joint_positions(
-        self,
-        positions: Optional[np.ndarray],
-        robot_indices: Optional[Union[np.ndarray, List]] = None,
-        joint_indices: Optional[Union[np.ndarray, List]] = None,
-    ) -> None:
-        """
-        Sets the joint positions of all robots specified in robot_indices to their respective values specified in positions.
-        """
-        pass
-    
-    @abstractmethod
-    def set_joint_position_targets(
-        self,
-        positions: Optional[np.ndarray],
-        robot_indices: Optional[Union[np.ndarray, List]] = None,
-        joint_indices: Optional[Union[np.ndarray, List]] = None,
-    ) -> None:
-        """
-        Sets the joint position targets of all robots specified in robot_indices to their respective values specified in positions.
-        """
-        pass
-
-    @abstractmethod
-    def set_joint_velocities(
-        self,
-        velocities: Optional[np.ndarray],
-        robot_indices: Optional[Union[np.ndarray, List]] = None,
-        joint_indices: Optional[Union[np.ndarray, List]] = None,
-    ) -> None:
-        """
-        Sets the joint velocities of all robots specified in robot_indices to their respective values specified in velocities.
-        """
-        pass
-     
-    @abstractmethod   
-    def set_joint_velocity_targets(
-        self,
-        velocities: Optional[np.ndarray],
-        robot_indices: Optional[Union[np.ndarray, List]] = None,
-        joint_indices: Optional[Union[np.ndarray, List]] = None,
-    ) -> None:
-        """
-        Sets the joint velocities targets of all robots specified in robot_indices to their respective values specified in velocities.
-        """
-        pass
-
-    @abstractmethod
-    def set_local_poses(
-        self,
-        translations: Optional[np.ndarray] = None,
-        orientations: Optional[np.ndarray] = None,
-        indices: Optional[Union[np.ndarray, List]] = None,
-    ) -> None:
-        """
-        Sets the local pose, meaning translation and orientation, of all objects (robots and obstacles)
-        """
-        pass
-
-    @abstractmethod
-    def get_local_poses(
-        self,
-        indices: Optional[Union[np.ndarray, List]] = None,
-    ) -> Tuple[np.ndarray, np.ndarray]:
-        """
-        Gets the local pose, meaning translation and orientation, of all objects (robots and obstacles)
-        """
-        pass
-
-    @abstractmethod
-    def get_collisions(self) -> List[Tuple[str, str]]:
-        """
-        Returns the ids of objects which are colliding. Updated after each step.
-        Example: [(Robot1, Robot2), (Robot1, Obstacle3)] -> Object 1 is colliding with object 2 and 3.
-        """
-        pass
 
     @abstractmethod
     def get_robot_dof_limits(self) -> np.ndarray:
@@ -106,6 +31,40 @@ class ModularEnv(VecEnv):
 
         """
         pass
+
+    @abstractmethod
+    def step_async(self, actions: np.ndarray) -> None:
+        pass
+    
+    @abstractmethod
+    def step_wait(self) -> VecEnvStepReturn:
+        pass
+
+    @abstractmethod
+    def reset(self) -> VecEnvObs:
+        pass
+
+    @abstractmethod
+    def close(self) -> None:
+        pass
+
+    def env_is_wrapped(self, wrapper_class: Type[gym.Wrapper], indices: VecEnvIndices = None) -> List[bool]:
+        # per default, modular envs don't support wrappper classes
+        return [False for _ in self._get_indices()]
+    
+    def env_method(self, method_name: str, *method_args, indices: VecEnvIndices = None, **method_kwargs) -> List[Any]:
+        return [getattr(i, method_name)(*method_args, *method_kwargs) for i in self._get_indices(indices)]
+    
+    def get_attr(self, attr_name: str, indices: VecEnvIndices = None) -> List[Any]:
+        return [self.env_data[i][attr_name] for i in self._get_indices(indices)]
+
+    def set_attr(self, attr_name: str, value: Any, indices: VecEnvIndices = None) -> None:
+        for i in self._get_indices(indices):
+            self.env_data[i][attr_name] = value
+
+    def seed(self, seed: Optional[int] = None) -> List[Union[None, int]]:
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
 
     def _get_absolute_asset_path(self, urdf_path: str):
         return Path(self.asset_path).joinpath(urdf_path)
--- spawnables/robot.py
+++ spawnables/robot.py
@@ -5,7 +5,7 @@ from spawnables.spawnable import Spawnable
 
 class Robot(Spawnable):
     def __init__(self, position: ndarray, orientation: ndarray, urdf_path:str, mass: float, color: List[float], collision: bool, observable: bool,
-                  observable_joints: str="", controlable_joints: str="*") -> None:
+                  observable_joints: str="") -> None:
         """
         position: Beginning position of robot
         orientation: Beginning orientation of robot
@@ -15,11 +15,9 @@ class Robot(Spawnable):
         collision: True if the robot is supposed to collide with surroundings
         observable: True if the robots position and orientation is included in the observations for RL training
         observable_joints: Regex matching joint names whose relative positions and orientation must be included in observations. Defaults to none.
-        controlable_joints: Regex matching joint names who can be controlled by the RL agent. Defaults to all.
         """
         super().__init__(position, mass, color, collision, observable)
         self.orientation = orientation
         self.urdf_path = urdf_path
 
         self.observable_joints = observable_joints
-        self.controlable_joints = controlable_joints
