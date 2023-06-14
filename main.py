70c47cbfe9277d03ade3ae401caaddd05024ba2e
erik.fischer98 <erik.fischer@campus.tu-berlin.de>
2023-06-14 11:48:54 +0200

    implemented parsing of required parameters

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from scripts.envs.params.config_parser import parse_config
 from scripts.spawnables.robot import Robot
 
-# print(parse_config('./data/configs/example.yaml').__dict__)
-robot = Robot("213", orientation=[1.1, 2, 3])
+print(parse_config('./data/configs/example.yaml').__dict__)
--- scripts/envs/params/config_parser.py
+++ scripts/envs/params/config_parser.py
@@ -5,6 +5,9 @@ from scripts.spawnables.robot import Robot
 from scripts.spawnables.obstacle import *
 from scripts.rewards.reward import Reward
 from scripts.rewards.distance import Distance
+from scripts.resets.reset import Reset
+from scripts.resets.distance_reset import DistanceReset
+from scripts.resets.timesteps_reset import TimestepsReset
 
 
 def parse_config(path: str) -> EnvParams:
@@ -17,7 +20,9 @@ def parse_config(path: str) -> EnvParams:
         robots = [_parse_robot(params) for params in _parse_params(content["robots"])]
         obstacles = [_parse_obstacle(params) for params in _parse_params(content["obstacles"])]
         rewards = [_parse_reward(params) for params in _parse_params(content["rewards"])]
-        resets = []
+        
+        # parsing name is not required since reset conditions have no name
+        resets = [_parse_reset(params) for params in content["resets"].values()]
 
         # parse optional parameters
         return EnvParams(engine, robots, obstacles, rewards, resets)
@@ -69,6 +74,25 @@ def _parse_reward(params: dict) -> Reward:
     # make sure parsing of obstacle type is implemented
     if type not in selector:
         raise Exception(f"Reward parsing of {type} is not implemented")
+    
+    # remove type parameter from dict to allow passing params directly to constructor
+    params.pop("type")
+
+    # return instance of parsed reward
+    return selector[type](**params)
+
+def _parse_reset(params: dict) -> Reset:
+    selector = {
+        "DistanceReset": DistanceReset,
+        "TimestepsReset": TimestepsReset
+    }
+
+    # extract required type
+    type = params["type"]
+
+    # make sure parsing of obstacle type is implemented
+    if type not in selector:
+        raise Exception(f"Reset parsing of {type} is not implemented")
     
     # remove type parameter from dict to allow passing params directly to constructor
     params.pop("type")
