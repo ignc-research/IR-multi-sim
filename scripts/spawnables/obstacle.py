6e158e2b7b5f3ce0aaca414f2ef1588b1b8e426b
erik.fischer98 <erik.fischer@campus.tu-berlin.de>
2023-06-30 07:54:37 +0200

    added documentation for dynamic obstacles

--- scripts/spawnables/obstacle.py
+++ scripts/spawnables/obstacle.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import List
 from numpy import ndarray, array
 from scripts.spawnables.spawnable import Spawnable
 
@@ -18,6 +18,17 @@ class Cube(Obstacle):
         observable: bool = True,
         name: str=None
     )-> None:
+        """A cube obstacle spawned in each environment with fixed parameters.
+
+        Args:
+            position (ndarray, optional): Position of cube. Defaults to array([0, 0, 0]).
+            orientation (ndarray, optional): Orientation of cube in quaternion. Defaults to array([1, 0, 0, 0]).
+            scale (List[float], optional): Scale of cube. Defaults to [1., 1., 1.].
+            color (ndarray, optional): Color of cube in RGB. Defaults to array([1., 1., 1.]).
+            collision (bool, optional): Flag for enabeling collision. Defaults to True.
+            observable (bool, optional): Flag for enabeling observability by ML model. Defaults to True.
+            name (str, optional): Name of cube, allowing it to be referenced. Defaults to None.
+        """
         super().__init__(position, color, collision, observable, name)
 
         # parse orientation
@@ -38,6 +49,16 @@ class Sphere(Obstacle):
         observable: bool = True,
         name: str=None
     ) -> None:
+        """A sphere obstacle spawned in each environment with fixed parameters.
+
+        Args:
+            position (ndarray, optional): Position of the sphere. Defaults to array([0, 0, 0]).
+            radius (float, optional): Radius of the sphere. Defaults to 1..
+            color (ndarray, optional): Color of the sphere in RGB. Defaults to array([1., 1., 1.]).
+            collision (bool, optional): Flag for enabeling collision. Defaults to True.
+            observable (bool, optional): Flag for enabeling observability by ML model. Defaults to True.
+            name (str, optional): Name of sphere, allowing it to be referenced. Defaults to None.
+        """
         super().__init__(position, color, collision, observable, name)
         self.radius = radius
 
@@ -52,6 +73,18 @@ class Cylinder(Obstacle):
         observable: bool = True,
         name: str=None
     ) -> None:
+        """A cylinder obstacle spawned in each environment with fixed parameters.
+
+        Args:
+            position (ndarray, optional): Position of the cylinder. Defaults to array([0, 0, 0]).
+            radius (float, optional): Radius of the cylinder. Defaults to 1..
+            height (float, optional): Height of the cylinder. Defaults to 2..
+            color (ndarray, optional): Color of the cylinder in RGB.. Defaults to array([1., 1., 1.]).
+            collision (bool, optional): Flag for enabeling collision. Defaults to True.
+            observable (bool, optional): Flag for enabeling observability by ML model. Defaults to True.
+            name (str, optional): Name of cylinder, allowing it to be referenced. Defaults to None.
+        """
+
         super().__init__(position, color, collision, observable, name)
         self.radius = radius
         self.height = height
